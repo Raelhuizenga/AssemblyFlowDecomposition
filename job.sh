2ac7eefc0d43ce008fb15a12f8c92d580c3b3389
Rael Huizenga <raelhuizenga@gmail.com>
2025-02-20 11:47:41 +0100

    add mnt directory

--- job.sh
+++ job.sh
@@ -14,11 +14,11 @@ export APPTAINER_ROOT="."
 export APPTAINER_NAME="vg-flow-image.sif"
 
 # Get current working directory
-export WORKDIR="/tudelft.net/staff-umbrella/FlowDecomposition/flow_assembly/AssemblyFlowDecomposition"
+export WORKDIR="mnt/staff-umbrella/FlowDecomposition/flow_assembly/AssemblyFlowDecomposition"
 export SCRIPT_DIR="$WORKDIR/scripts"
 
 # Set Gurobi license file path
-export GRB_LICENSE_FILE="/tudelft.net/staff-umbrella/FlowDecomposition/gurobi.lic"
+export GRB_LICENSE_FILE="mnt/staff-umbrella/FlowDecomposition/gurobi.lic"
 
 # Run the first script inside the container
 srun apptainer exec \
